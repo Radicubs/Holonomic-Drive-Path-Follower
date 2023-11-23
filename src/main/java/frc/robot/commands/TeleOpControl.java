5b4f232219e672ee3c561d9c095622be71d1c746
Mihir Sood <soodmihir18@gmail.com>
2023-11-22 19:39:35 -0600

    added display functionality for poses and trajectories

--- src/main/java/frc/robot/commands/TeleOpControl.java
+++ src/main/java/frc/robot/commands/TeleOpControl.java
@@ -1,10 +1,18 @@
 package frc.robot.commands;
 
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
+import edu.wpi.first.math.trajectory.Trajectory;
+import edu.wpi.first.math.trajectory.TrajectoryConfig;
+import edu.wpi.first.math.trajectory.TrajectoryGenerator;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.Constants;
 import frc.robot.subsystems.HolonomicChassisSim;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.function.DoubleSupplier;
 
 public class TeleOpControl extends CommandBase {
@@ -18,6 +26,16 @@ public class TeleOpControl extends CommandBase {
         this.yInput = yInput;
         this.rotInput = rotInput;
         addRequirements(chassisSim);
+    }
+
+    @Override
+    public void initialize() {
+        Trajectory trajectory = TrajectoryGenerator.generateTrajectory(
+                new Pose2d(new Translation2d(2, 2), new Rotation2d()),
+                List.of(),
+                new Pose2d(new Translation2d(6, 3), new Rotation2d()),
+                new TrajectoryConfig(Constants.Simulation.MAX_AXIS_SPEED, Constants.Simulation.MAX_ACCELERATION));
+        chassisSim.displayTrajectory(trajectory);
     }
 
     @Override
